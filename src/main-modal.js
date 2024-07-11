66527ab95c64660ec8d2a07192138edf91aa3ff3
Cristian Frumusanu <cristian.frumusanu@autoxy.it>
2024-07-11 09:32:20 +0300

    Cleanup

--- src/main-modal.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './ModalApp.vue'
-
-createApp(App)
-    .mount('#dk-modal-container');
--- src/main-widget.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './WidgetApp.vue'
-
-createApp(App)
-    .mount('#dk-widget-container')
